fbfbfae37bd3babed0fc9d170e31c33d5ec537b3
Fernando Gonzalez <58346389+AFernandoGonzalez@users.noreply.github.com>
2021-09-04 09:29:41 -0400

    Part 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Fernando Gonzalez on 9/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
